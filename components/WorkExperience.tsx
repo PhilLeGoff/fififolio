f304e56939cf59e061e056fceaaedb9e48e6de6a
PhilLeGoff <webbyphil65@gmail.com>
2023-09-18 11:49:06 +0200

    m

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -12,7 +12,7 @@ function WorkExperience({}: Props) {
       transition={{ duration: 1.5 }}
       className="flex overflow-hidden relative flex-col justify-evenly items-center px-10 mx-auto max-w-full h-screen text-left md:flex-row"
     >
-      <h3 className="absolute top-16 md:top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-20 md:top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Experience
       </h3>
       <div className="w-full flex space-x-5 overflow-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
